2c441df0c8506835cfd3d11e6c5e7b732fc118ce
Martin Korotwitschka <korotwitschkamartin@gmail.com>
2023-09-28 19:21:23 +0200

    removed glm lib

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 //Include GLFW
 #include <GLFW/glfw3.h>
 //Include GLM
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+//#include <glm/vec3.hpp> // glm::vec3
+//#include <glm/vec4.hpp> // glm::vec4
+//#include <glm/mat4x4.hpp> // glm::mat4
+//#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+//#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
 
 //Include the standard C++ headers
 #include <stdlib.h>
@@ -95,8 +95,6 @@ const char* fragment_shader =
 
 int main(void)
 {
-
-
 	GLFWwindow* window;
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit()) {
