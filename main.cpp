ee4394e8751c232fdf5d398eb3a4389970abf635
Martin Korotwitschka <korotwitschkamartin@gmail.com>
2023-09-28 23:05:22 +0200

    its working

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ static void error_callback(int error, const char* description) { fputs(descripti
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-	printf("key_callback [%d,%d,%d,%d] \n", key, scancode, action, mods);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    printf("key_callback [%d,%d,%d,%d] \n", key, scancode, action, mods);
 }
 
 static void window_focus_callback(GLFWwindow* window, int focused) { printf("window_focus_callback \n"); }
@@ -28,18 +28,18 @@ static void window_focus_callback(GLFWwindow* window, int focused) { printf("win
 static void window_iconify_callback(GLFWwindow* window, int iconified) { printf("window_iconify_callback \n"); }
 
 static void window_size_callback(GLFWwindow* window, int width, int height) {
-	printf("resize %d, %d \n", width, height);
-	glViewport(0, 0, width, height);
+    printf("resize %d, %d \n", width, height);
+    glViewport(0, 0, width, height);
 }
 
 static void cursor_callback(GLFWwindow* window, double x, double y) { printf("cursor_callback \n"); }
 
 static void button_callback(GLFWwindow* window, int button, int action, int mode) {
-	if (action == GLFW_PRESS) {
-		printf("button_callback [%d,%d,%d]\n", button, action, mode);
+    if (action == GLFW_PRESS) {
+        printf("button_callback [%d,%d,%d]\n", button, action, mode);
 
 
-	}
+    }
 
 }
 
@@ -50,155 +50,158 @@ static void button_callback(GLFWwindow* window, int button, int action, int mode
 
 
 float points[] = {
-	0.0f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-   -0.5f, -0.5f, 0.0f
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
 };
 
 //upravte si model na pozici a barvu a zprovozněte prosím (nápovědu vidíte)
 
 struct point
 {
-	float pos[4];
-	float color[4];
+    float pos[4];
+    float color[4];
 };
 
 const point b[] = {
-   { { -.5f, -.5f, .5f, 1 }, { 1, 1, 0, 1 } },
-   { { -.5f, .5f, .5f, 1 }, { 1, 0, 0, 1 } },
-   { { .5f, .5f, .5f, 1 }, { 0, 0, 0, 1 } },
-   { { .5f, -.5f, .5f, 1 }, { 0, 1, 0, 1 } },
+        { { -.5f, -.5f, .5f, 1 }, { 1, 0, 0, 1 } },
+        { { -.5f, .5f, .5f, 1 }, { 0, 1, 0, 1 } },
+        { { .5f, .5f, .5f, 1 }, { 0, 0, 1, 1 } },
+        { { .5f, -.5f, .5f, 1 }, { 1, 1, 1, 1 } },
 };
 
 
 const char* vertex_shader =
-"#version 330\n"
-"layout(location=0) in vec3 vp;"
-"void main () {"
-"     gl_Position = vec4 (vp, 1.0);"
-"}";
-
-
-
+        "#version 330\n"
+        "layout(location=0) in vec4 position;"
+        "layout(location=1) in vec4 vertex_Color;"
+        "out vec4 colorInput;"
+        "void main () {"
+        "     gl_Position = position;"
+        "     colorInput = vertex_Color;"
+        "}";
 
 
 const char* fragment_shader =
-"#version 330\n"
-"out vec4 frag_colour;"
-"in vec4 vertex_Color;"
-"void main () {"
-"     frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
-"}";
+        "#version 330\n"
+        "in vec4 colorInput;"
+        "out vec4 color;"
+        "void main () {"
+        "     color = colorInput;"
+        "}";
 
 
 
 
-int main(void)
+int main()
 {
-	GLFWwindow* window;
-	glfwSetErrorCallback(error_callback);
-	if (!glfwInit()) {
-		fprintf(stderr, "ERROR: could not start GLFW3\n");
-		exit(EXIT_FAILURE);
-	}
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        exit(EXIT_FAILURE);
+    }
 
-	/* //inicializace konkretni verze
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE,
-	GLFW_OPENGL_CORE_PROFILE);  //*/
+    /* //inicializace konkretni verze
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,
+    GLFW_OPENGL_CORE_PROFILE);  //*/
 
-	window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
-	if (!window) {
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
+    window = glfwCreateWindow(600, 600, "ZPG", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit();
-
-
-
-
-	// get version info
-	printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
-	printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
-	printf("Vendor %s\n", glGetString(GL_VENDOR));
-	printf("Renderer %s\n", glGetString(GL_RENDERER));
-	printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-	int major, minor, revision;
-	glfwGetVersion(&major, &minor, &revision);
-	printf("Using GLFW %i.%i.%i\n", major, minor, revision);
-
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	float ratio = width / (float)height;
-	glViewport(0, 0, width, height);
-
-
-	//vertex buffer object (VBO)
-	GLuint VBO = 0;
-	glGenBuffers(1, &VBO); // generate the VBO
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(b), b, GL_STATIC_DRAW);
-
-	//Vertex Array Object (VAO)
-	GLuint VAO = 0;
-	glGenVertexArrays(1, &VAO); //generate the VAO
-	glBindVertexArray(VAO); //bind the VAO
-	glEnableVertexAttribArray(0); //enable vertex attributes
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(b[0]), (GLvoid*)0);
-
-
-	//create and compile shaders
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertex_shader, NULL);
-	glCompileShader(vertexShader);
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
-	glCompileShader(fragmentShader);
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, fragmentShader);
-	glAttachShader(shaderProgram, vertexShader);
-	glLinkProgram(shaderProgram);
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
 
 
 
-	GLint status;
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
-		glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
-		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
-		delete[] strInfoLog;
-	}
+
+    // get version info
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+    printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    printf("Vendor %s\n", glGetString(GL_VENDOR));
+    printf("Renderer %s\n", glGetString(GL_RENDERER));
+    printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+    printf("Using GLFW %i.%i.%i\n", major, minor, revision);
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
 
 
-	while (!glfwWindowShouldClose(window)) {
-		// clear color and depth buffer
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		// draw triangles
-		glDrawArrays(GL_POLYGON, 0, 4); //mode,first,count
-		// update other events like input handling
-		glfwPollEvents();
-		// put the stuff we’ve been drawing onto the display
-		glfwSwapBuffers(window);
-	}
-	glfwDestroyWindow(window);
+    //vertex buffer object (VBO)
+    GLuint VBO = 0;
+    glGenBuffers(1, &VBO); // generate the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(b), b, GL_STATIC_DRAW);
 
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+
+    //Vertex Array Object (VAO)
+    GLuint VAO = 0;
+    glGenVertexArrays(1, &VAO); //generate the VAO
+    glBindVertexArray(VAO); //bind the VAO
+    glEnableVertexAttribArray(0); //enable vertex attributes
+    glEnableVertexAttribArray(1); //enable vertex attributes
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(b[0]), (GLvoid*)0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(b[0]), (GLvoid*)16);
+//    glColorPointer(4, GL_FLOAT, sizeof(b[0]), (GLvoid*)16);
+
+    //create and compile shaders
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader);
+    glLinkProgram(shaderProgram);
+
+
+
+    GLint status;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE)
+    {
+        GLint infoLogLength;
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
+
+
+    while (!glfwWindowShouldClose(window)) {
+        // clear color and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        // draw triangles
+        glDrawArrays(GL_POLYGON, 0, 4); //mode,first,count
+        // update other events like input handling
+        glfwPollEvents();
+        // put the stuff we’ve been drawing onto the display
+        glfwSwapBuffers(window);
+    }
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
-
 
